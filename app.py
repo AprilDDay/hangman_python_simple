1643dcb0f5944445e87bff26f8f1520878e4f00e
AprilDDay <61091062+AprilDDay@users.noreply.github.com>
2021-08-24 22:45:50 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,25 @@
 
 def hangman(word): 
   wrong = 0
-  stages = ["", "____________ ", "| ", "| | ", "| 0 ", "| /|\ ", "| / \ ", "|  "]
+  stages = ["", "__________ ", "| ", "| | ", "| 0 ", "| /|\ ", "| / \ ", "|  "]
   rletters = list(word)
   board = ["_"] * len(word)
   win = False
   print("Welcome to HANGMAN!")
+
+ while wrong < len(stages)-1:
+  print("\n")
+  msg = "Guess a letter in the word..." 
+  char = input(msg)
+  if char in rletters: 
+    cind = rletters \ .index(char)
+    board[cind] = char 
+    #rletters[cind] = 
+  
+  #need to put if win
+  
+  if not win: 
+    print("\n" .join(stages[0: \ wrong]))
+    print("Oh no! You lost! It was {}.".format(word))
+    
+ 
